93412d16313fcc22b31ece060ce50369461c62ab
ducvinh <ducvinh100503@gmail.com>
2023-12-29 21:41:17 +0700

    scrape product

--- scrapeController.js
+++ scrapeController.js
@@ -7,26 +7,45 @@ const scrapeController = async(browserInstance) => {
         let browser = await browserInstance
         let categorys = await scrapers.scrapeCategory(browser, url)
 
-        const data = []
+        const resultPage = await scrapers.scrape(browser, categorys[0].link)
 
-        for (let i = 0; i < 8; i++) {
-            const itemLinks = await scrapers.scrape(browser, categorys[i].link)
+        const resultDetail = await scrapers.scrapeDetail(browser, resultPage[0])
 
-            for (let itemLink of itemLinks) {
-                if (itemLink == 'https://cellphones.com.vn/samsung-galaxy-a15.html') continue;
-                else {
-                    const response = await scrapers.scrapeDetail(browser, itemLink)
+        console.log(resultDetail)
+        
+        // const data = []
 
-                    data.push(response)
-                }
-            }
-        }
+        // for (let i = 0; i < categorys.length; i++) {
+        //     const resultPages = await scrapers.scrape(browser, categorys[i].link)
 
-        fs.writeFile('ecommerce.json', JSON.stringify(data), (err) => {
-            if (err) console.log("Ghi data vo file json error: " + err)
-            console.log('Add data success')
-        })
+        //     for (let resultPage of resultPages) {
+        //         if (resultPage == 'https://cellphones.com.vn/samsung-galaxy-a15.html' 
+        //             || resultPage == 'https://cellphones.com.vn/samsung-galaxy-s22-ultra-12gb-256gb.html'
+        //             || resultPage == 'https://cellphones.com.vn/samsung-galaxy-z-fold-5-256gb.html'
+        //             || resultPage == 'https://cellphones.com.vn/xiaomi-mi-pad-5-256gb.html'
+        //             || resultPage == 'https://cellphones.com.vn/ipad-10-2-inch-2021.html'
+        //             || resultPage == 'https://cellphones.com.vn/samsung-galaxy-tab-a9.html'
+        //             || resultPage == 'https://cellphones.com.vn/xiaomi-redmi-pad-se.html'
+        //             || resultPage == 'https://cellphones.com.vn/apple-airpods-2.html'
+        //             || resultPage == 'https://cellphones.com.vn/apple-airpods-pro-2-usb-c.html'
+        //             || resultPage == 'https://cellphones.com.vn/vong-deo-tay-thong-minh-xiaomi-mi-band-8.html'
+        //             || resultPage == 'https://cellphones.com.vn/vong-deo-tay-thong-minh-xiaomi-mi-band-8-active.html'
+        //             || resultPage == 'https://cellphones.com.vn/rog-ally-z1.html'
+        //             || resultPage == 'https://cellphones.com.vn/asus-rog-ally.html'
+        //             || resultPage == 'https://cellphones.com.vn/gia-treo-man-hinh-may-tinh-north-bayou-nb-f80.html'
+        //             || resultPage == 'https://cellphones.com.vn/gia-treo-man-hinh-may-tinh-north-bayou-nb-f160.html') continue;
+        //         else {
+        //             const resultDetail = await scrapers.scrapeDetail(browser, resultPage, i)
+        //             if (resultDetail === false) continue
+        //             else data.push(resultDetail)
+        //         }
+        //     }
+        // }
 
+        // fs.writeFile('products.json', JSON.stringify(data), (err) => {
+        //     if (err) console.log("Ghi data vo file json error: " + err)
+        //     console.log('Add data success')
+        // })
     } catch(e) {
         console.log("Error in scrape controller: "+ e)
     }
--- scraper.js
+++ scraper.js
@@ -11,19 +11,47 @@ const scrapeCategory = (browser, url) => new Promise(async(resolve, reject) => {
 
         const dataCategory = await page.$$eval('#menu-main > div.menu-wrapper > div.menu-tree > div.label-menu-tree', els => {
             dataCategory = els.map(el => {
-                return {
-                    category: el.querySelector('a').innerText,
-                    link: el.querySelector('a').href,
+                const check = el.querySelectorAll('a')
+
+                if (check.length > 1) {
+                    return [
+                        {
+                            category: el.querySelectorAll('span')[0].innerText,
+                            link: el.querySelectorAll('a')[0].href
+                        },
+                        {
+                            category: el.querySelectorAll('span')[1].innerText,
+                            link: el.querySelectorAll('a')[1].href
+                        }
+                    ]
+                } else {
+                    return {
+                        category: el.querySelector('span').innerText,
+                        link: el.querySelector('a').href
+                    }
                 }
             })
 
             return dataCategory
         })
 
-        // console.log(dataCategory)
+        const data = []
+
+        for (let i = 0; i < 8; i++) {
+            if(dataCategory[i][0]) {
+                if (i == 6) {
+                    data.push(dataCategory[i][1])
+                } else {
+                    data.push(dataCategory[i][0])
+                    data.push(dataCategory[i][1])
+                }
+            } else {
+                data.push(dataCategory[i])
+            }
+        }
 
         await page.close()
-        resolve(dataCategory)
+        resolve(data)
     } catch(e) {
         console.log("Error in scrape category: "+ e)
         reject()
@@ -65,7 +93,7 @@ const scrape = (browser, url) => new Promise(async(resolve, reject) => {
     }
 })
 
-const scrapeDetail = (browser, url) => new Promise(async(resolve, reject) => {
+const scrapeDetail = (browser, url, i) => new Promise(async(resolve, reject) => {
     try {
         // new page
         let page = await browser.newPage()
@@ -78,11 +106,71 @@ const scrapeDetail = (browser, url) => new Promise(async(resolve, reject) => {
 
         const detailData = {}
 
+        switch(i) {
+            case 0: 
+                detailData.category = "smartphone"
+                break;
+            case 1: 
+                detailData.category = "tablet"
+                break;
+            case 2: 
+                detailData.category = "laptop"
+                break;
+            case 3: 
+                detailData.category = "audio"
+                break;
+            case 4: 
+                detailData.category = "watch"
+                break;
+            case 5: 
+                detailData.category = "camera"
+                break;
+            case 6: 
+                detailData.category = "appliances"
+                break;
+            case 7: 
+                detailData.category = "smarthome"
+                break;
+            case 8: 
+                detailData.category = "accessory"
+                break;
+            case 9: 
+                detailData.category = "monitor"
+                break;
+            case 10: 
+                detailData.category = "tivi"
+                break;
+        }
+
+        if (i !== 3 && i !== 5 && i !== 6 && i !== 7 && i !== 8 && i !== 10) {
+            const getPrice = await page.$eval('#trade-price-tabs > div.tpt-wrapper > div.tpt-boxs > div.has-text-centered', (el) => {
+                return {
+                    sale: el.querySelector('p.tpt---sale-price')?.innerText,
+                    price: el.querySelector('p.tpt---price')?.innerText,
+                }
+            })
+            detailData.price = getPrice
+        } else {
+            const getPrice2 = await page.$eval('#productDetailV2 > section.block-detail-product > div.box-detail-product > div.box-detail-product__box-center > div.block-box-price > div.box-info__box-price', (el) => {
+                return {
+                    sale: el.querySelector('p.product__price--show')?.innerText,
+                    price: el.querySelector('p.product__price--through')?.innerText,
+                }
+            })
+            detailData.price = getPrice2
+        }
+        
+        const getIncentives = await page.$$eval('#productDetailV2 > section.block-detail-product > div.box-detail-product > div.box-detail-product__box-center > div.box-product-promotion > div.box-product-promotion-content > div', (els) => {
+            return els.map(el => {
+                return el.querySelector('a')?.innerHTML
+            })
+        })
+        detailData.incentives = getIncentives
+
         const getName = await page.$eval('#productDetailV2 > section.block-detail-product > div.box-header > div.box-product-name', (el) => {
             return el.querySelector('h1')?.innerText
         })
         detailData.name = getName
-
         detailData.brand = getName.split(' ')[0]
 
         const images = await page.$$eval('section.block-detail-product > div.box-detail-product > div.box-detail-product__box-left > div.box-gallery > div.gallery-product-detail > div.gallery-slide > div.swiper-wrapper > div.swiper-slide', (els) => {
@@ -91,17 +179,7 @@ const scrapeDetail = (browser, url) => new Promise(async(resolve, reject) => {
             })
         })
         detailData.images = images
-
-        const description = await page.$$eval('section.block-detail-product > div.block-content-product > div.block-content-product-right > div > div.cps-block-technicalInfo > ul > li', (els) => {
-            return els.map(el => {
-                return {
-                    key: el.querySelector('p').innerText,
-                    value: el.querySelector('div').innerText,
-                }
-            })
-        })
-        detailData.description = description
-
+        
         const variants = await page.$$eval('#productDetailV2 > section.block-detail-product > div.box-detail-product > div.box-detail-product__box-center > div.box-product-variants > div.box-content > ul > li', (els) => {
             return els.map(el => {
                 return {
@@ -115,14 +193,14 @@ const scrapeDetail = (browser, url) => new Promise(async(resolve, reject) => {
 
         const information = await page.$$eval('#productDetailV2 > section.block-detail-product > div.box-detail-product > div.box-detail-product__box-left > div.columns > div.column > div.box-warranty-info > div.box-content > div.item-warranty-info', (els) => {
             return els.map(el => {
-                return el.querySelector('div.description').innerText
+                return el.querySelector('div.description')?.innerText
             })
         })
         detailData.information = information
 
         const highlights = await page.$$eval('#productDetailV2 > section.block-detail-product > div.block-content-product > div.block-content-product-left > div.cps-block-content > div.ksp-content > div > ul > li', (els) => {
             return els.map(el => {
-                return el.innerText
+                return el?.innerText
             })
         })
         detailData.highlights = highlights
@@ -139,13 +217,17 @@ const scrapeDetail = (browser, url) => new Promise(async(resolve, reject) => {
         const version = await page.$$eval('#productDetailV2 > section.block-detail-product > div.box-detail-product > div.box-detail-product__box-center > div.box-linked > div.list-linked > a', (els) => {
             return els.map(el => {
                 return {
-                    data: el.querySelector('strong').innerText,
-                    price: el.querySelector('span').innerText
+                    data: el.querySelector('strong')?.innerText,
+                    price: el.querySelector('span')?.innerText
                 }
             })
         })
         detailData.version = version
 
+        const getDescription = await page.$eval('#productDetailV2 > section.block-detail-product > div.block-content-product > div.block-content-product-right', (el) => {
+            return el.innerHTML
+        })
+        detailData.description = getDescription
 
         await page.close()
         resolve(detailData)
